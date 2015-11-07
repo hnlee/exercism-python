fa0adbc79cf6d703fedf529c9f20deb036e446f9
Hana Lee <hanalee07@gmail.com>
2015-11-07 11:39:20 -0600

    Replaced list(...) with [...] notation

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,4 +1,4 @@
 def detect_anagrams(word, candidates):
-    return list(anagram for anagram in candidates
+    return [anagram for anagram in candidates
         if sorted(anagram.lower()) == sorted(word.lower()) and
-        anagram.lower() != word.lower())
+        anagram.lower() != word.lower()]
